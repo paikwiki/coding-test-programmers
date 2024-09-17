4b6f057eebefce5e8f02b88cb279b898e27c1dd0
paikwiki <paikwiki@gmail.com>
2024-09-17 20:04:10 +0900

    :children_crossing: Can execute 'test' without filename

--- src/index.js
+++ src/index.js
@@ -1,15 +1,45 @@
 import * as R from "ramda";
+import fs from "fs";
+import path from "path";
+
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
+
+const quizzesDir = path.join(__dirname, "quizzes");
 
 const fileName = process.argv[2];
 
-if (!fileName) {
-  console.error("Error: FILE_NAME argument is required.");
-  process.exit(1);
+function getLatestFile() {
+  const files = fs.readdirSync(quizzesDir);
+
+  const jsFiles = files.filter(
+    (file) => file.endsWith(".js") && !file.endsWith("-test-cases.js")
+  );
+
+  if (jsFiles.length === 0) {
+    console.error(
+      "Error: No suitable .js files found in the quizzes directory."
+    );
+    process.exit(1);
+  }
+
+  const latestFile = jsFiles
+    .map((file) => ({
+      file,
+      time: fs.statSync(path.join(quizzesDir, file)).mtime.getTime(),
+    }))
+    .sort((a, b) => b.time - a.time)[0].file;
+
+  return latestFile.replace(".js", "");
 }
 
-const { solution } = await import(`./quizzes/${fileName}.js`);
-const { testCases } = await import(`./quizzes/${fileName}-test-cases.js`);
+const selectedFileName = fileName || getLatestFile();
 
+const { solution } = await import(`./quizzes/${selectedFileName}.js`);
+const { testCases } = await import(
+  `./quizzes/${selectedFileName}-test-cases.js`
+);
+
+console.log(`🤖 execute: ./quizzes/${selectedFileName}.js\n`);
 testCases.forEach(({ input, output }, index) => {
   const result = solution(...input);
   const isEqual = R.equals(result, output);
